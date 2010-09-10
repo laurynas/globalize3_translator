374f527ab67e4102d654c712b01e49d70437b3b7
Laurynas Butkus <laurynas.butkus@gmail.com>
2010-09-10 11:19:56 +0300

    Test modifying

--- test/translator/translator_test.rb
+++ test/translator/translator_test.rb
@@ -35,18 +35,15 @@ class TranslatorTest < ActiveSupport::TestCase
   end
   
   test "Should translate data from default locale when saving" do
-    I18n.locale = :en
-    
     post = Post.new( :title => "Dog", :content => "Cat" )
     
     assert post.save!
     assert post.reload
-    #post = Post.find :last
     
     assert_equal "Dog",         post.title
     assert_equal "Cat",         post.content
-    assert_equal false,         post.title.auto_translated?
-    assert_equal false,         post.content.auto_translated?
+    assert !post.title.auto_translated?
+    assert !post.content.auto_translated?
     
     I18n.locale = :lt
     
@@ -58,6 +55,39 @@ class TranslatorTest < ActiveSupport::TestCase
     I18n.locale = :cs
     
     assert_equal "Pes",         post.title
+    assert post.title.auto_translated?
+  end
+  
+  test "Modifying model" do
+    post = Post.new( :title => "Dog", :content => "Cat" )
+    assert post.save!
+    
+    assert post = Post.find(:last)
+    
+    I18n.locale = :lt
+    
+    assert_equal "Šuo", post.title
+    assert post.title.auto_translated?
+    
+    post.title = "Vilkas"
+    
+    assert_equal "Vilkas", post.title
+    assert !post.title.auto_translated?
+    
+    assert post.save!
+    
+    assert post = Post.find(:last)
+    
+    assert !post.title.auto_translated?
+    
+    I18n.locale = :en
+    
+    assert_equal "Wolf", post.title
+    assert post.title.auto_translated?
+    
+    I18n.locale = :cs
+    
+    assert_equal "Vlk", post.title
     assert post.title.auto_translated?
   end
   
